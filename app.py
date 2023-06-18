9d1c4a2b7102913c4bf53041b8e99c683cd4d0d3
Ebin Babu Thomas <47640932+ebt15@users.noreply.github.com>
2023-06-18 20:16:10 +0530

    added progress bar and removed streamlit branding

--- app.py
+++ app.py
@@ -22,13 +22,31 @@ class VideoSummarizer:
         with open(save_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
 
-    def process_video(self, video_path):
+    def process_video(self, video_path, progress_bar, progress_text):
+        progress_text.text("Processing video...")
         wav_file = self.video_processor.video_to_wav(video_path)
-        transcript = self.whisper_transcriber.transcribe_audio(wav_file)
-        return self.summary_generator.summarize_document(transcript)
+        progress_bar.progress(33)
 
+        progress_text.text("Transcribing audio...")
+        transcript = self.whisper_transcriber.transcribe_audio(wav_file)
+        progress_bar.progress(66)
+
+        progress_text.text("Generating summary...")
+        summary = self.summary_generator.summarize_document(transcript)
+        progress_bar.progress(100)
+
+        return summary
 
 def main():
+    # Hide Streamlit branding
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    
     # Create the "uploaded_videos" folder if it doesn't exist
     if not os.path.exists("uploaded_videos"):
         os.makedirs("uploaded_videos")
@@ -46,13 +64,17 @@ def main():
 
         st.success(f"File saved to {save_path}")
 
+        # Add a progress bar and progress text
+        progress_bar = st.progress(0)
+        progress_text = st.empty()
+
         # Process the video and generate the summary
-        summary = video_summarizer.process_video(save_path)
+        summary = video_summarizer.process_video(save_path, progress_bar, progress_text)
 
         # Display the summary
         st.header("Summary")
         st.write(summary)
-
+        progress_text.text("Done!")
 
 if __name__ == "__main__":
     main()
