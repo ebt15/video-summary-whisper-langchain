3114cf972ed552605d3585a5005f69cb4547c6e8
Ebin Babu Thomas <47640932+ebt15@users.noreply.github.com>
2023-06-20 00:10:45 +0530

    added summary type feature and transcript download feature

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import streamlit as st
 from src.video_processing import VideoProcessor
 from src.whisper_transcription import WhisperTranscriber
 from src.summary_generation import SummaryGenerator
+import base64
+from streamlit import components
 
 load_dotenv()
 
@@ -21,8 +23,23 @@ class VideoSummarizer:
     def save_uploaded_file(uploaded_file, save_path):
         with open(save_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
+            
+    @staticmethod
+    def create_auto_download_link(content, filename):
+        b64_content = base64.b64encode(content.encode()).decode()
+        js_code = f'''
+            function download_transcript() {{
+                var link = document.createElement('a');
+                link.href = "data:text/plain;base64,{b64_content}";
+                link.download = "{filename}";
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }}
+        '''
+        return f'<script>{js_code}</script><button onclick="download_transcript()">Download Transcript</button>'
 
-    def process_video(self, video_path, progress_bar, progress_text):
+    def process_video(self, video_path, progress_bar, progress_text, summary_type):
         progress_text.text("Processing video...")
         wav_file = self.video_processor.video_to_wav(video_path)
         progress_bar.progress(33)
@@ -32,10 +49,10 @@ class VideoSummarizer:
         progress_bar.progress(66)
 
         progress_text.text("Generating summary...")
-        summary = self.summary_generator.summarize_document(transcript)
+        summary = self.summary_generator.summarize_document(transcript, summary_type)
         progress_bar.progress(100)
 
-        return summary
+        return summary, transcript
 
 def main():
     # Hide Streamlit branding
@@ -46,7 +63,7 @@ def main():
     </style>
     """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    
+
     # Create the "uploaded_videos" folder if it doesn't exist
     if not os.path.exists("uploaded_videos"):
         os.makedirs("uploaded_videos")
@@ -55,6 +72,9 @@ def main():
 
     # Streamlit code for UI
     st.title("Video Summarizer")
+        
+    # Add a select box for summary type
+    summary_type = st.selectbox("Select Summary Type", ["Detailed", "Concise"])
 
     uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov", "mkv"])
 
@@ -69,12 +89,16 @@ def main():
         progress_text = st.empty()
 
         # Process the video and generate the summary
-        summary = video_summarizer.process_video(save_path, progress_bar, progress_text)
+        summary, transcript = video_summarizer.process_video(save_path, progress_bar, progress_text, summary_type)
 
         # Display the summary
         st.header("Summary")
         st.write(summary)
         progress_text.text("Done!")
+
+        # Provide a feature to download the transcript
+        download_link = video_summarizer.create_auto_download_link(transcript, "transcript.txt")
+        components.v1.html(download_link, height=50)
 
 if __name__ == "__main__":
     main()
--- src/summary_generation.py
+++ src/summary_generation.py
@@ -1,10 +1,9 @@
-import pathlib
 import textwrap
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
-
+from langchain.prompts import PromptTemplate
 
 class SummaryGenerator:
     def __init__(self, model='gpt-3.5-turbo', temperature=0, chunk_size=8000, chunk_overlap=20):
@@ -12,34 +11,69 @@ class SummaryGenerator:
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
 
     def load_document(self, document_text):
-        # Load the document
-        # document_text = pathlib.Path(file_path).read_text()
+        # Load the document texts
         texts = self.text_splitter.split_text(document_text)
 
         return [Document(page_content=t) for t in texts[:4]]
 
-    def generate_summary(self, docs):
+    def generate_summary(self, docs, summary_type="concise"):
+        if summary_type == "detailed":
+            return self.generate_detailed_summary(docs)
         # Load the summarization chain
         chain = load_summarize_chain(self.llm, chain_type="refine")
 
         return chain.run(docs)
 
+    def generate_detailed_summary(self, docs):
+        prompt_template = """Write a detailed summary of the following extracting the key information:
+
+                            {text}
+
+                            DETAILED SUMMARY:"""
+        PROMPT = PromptTemplate(template=prompt_template, 
+                                input_variables=["text"])
+
+        refine_template = (
+            "Your job is to produce a final summary\n"
+            "We have provided an existing summary up to a certain point: {existing_answer}\n"
+            "We have the opportunity to refine the existing summary"
+            "(only if needed) with some more context below.\n"
+            "------------\n"
+            "{text}\n"
+            "------------\n"
+            "Given the new context, refine the original summary"
+            "If the context isn't useful, return the original summary."
+        )
+        refine_prompt = PromptTemplate(
+            input_variables=["existing_answer", "text"],
+            template=refine_template,
+        )
+        chain = load_summarize_chain(self.llm, 
+                                     chain_type="refine", 
+                                     return_intermediate_steps=True, 
+                                     question_prompt=PROMPT, 
+                                     refine_prompt=refine_prompt,
+                                     verbose=True)
+
+        output_summary = chain({"input_documents": docs}, return_only_outputs=True)
+        return output_summary['output_text']
+
     def wrap_text(self, text, width=100):
         return textwrap.fill(text, width=width)
 
-    def summarize_document(self, document_text):
+    def summarize_document(self, document_text, summary_type="concise"):
         print("Loading document...")
         docs = self.load_document(document_text)
 
         print("Generating summary...")
-        summary = self.generate_summary(docs)
+        print(f"Summary Type {summary_type}")
+        summary = self.generate_summary(docs, summary_type)
 
         print("Wrapping text...")
         return self.wrap_text(summary)
 
-
 if __name__ == "__main__":
     summary_generator = SummaryGenerator()
-    wrapped_summary = summary_generator.summarize_document('meeting_doc.txt')
+    wrapped_summary = summary_generator.summarize_document('../explore/meeting_doc.txt', summary_type="detailed")
     print("Summary:")
     print(wrapped_summary)
